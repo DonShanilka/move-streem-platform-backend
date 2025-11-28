078307c6b5e9443788bbc86868b22749d82abe94
DonShanilka <shanilka799@gmail.com>
2025-11-28 22:34:33 +0530

    refactor: remove deprecated user repository and add new user repository implementation

---
+++ Admin_Dashboard/backend/services/auth-service/internal/repository/user_repository.go
@@ -0,0 +1,27 @@
+package repository
+
+import (
+	"context"
+	"github.com/DonShanilka/auth-service/internal/models"	
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type UserRepository struct {
+	Collection *mongo.Collection
+}
+
+func NewUserRepository(col *mongo.Collection) *UserRepository {
+	return &UserRepository{Collection: col}
+}
+
+func (r *UserRepository) CreateUser(user *models.User) error {
+	_, err := r.Collection.InsertOne(context.TODO(), user)
+	return err
+}
+
+func (r *UserRepository) FindUserByEmail(email string) (*models.User, error) {
+	var user models.User
+	err := r.Collection.FindOne(context.TODO(), bson.M{"email": email}).Decode(&user)
+	return &user, err
+}
