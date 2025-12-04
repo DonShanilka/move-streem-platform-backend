fa2f467e1bcee300cd5f0cff0812c044837f6aef
Shanilka800@# <shanilka799@gmail.com>
2025-12-04 23:29:25 +0530

    create SaveSeries in series_service

--- backend/services/movie-service/internal/models/series.go
+++ backend/services/movie-service/internal/models/series.go
@@ -1,16 +1,14 @@
 package models
 
-import "time"
 
 type Series struct {
-    ID           int       `json:"id" db:"id"`
-    Title        string    `json:"title" db:"title"`
-    Description  string    `json:"description" db:"description"`
-    ReleaseYear  *int      `json:"release_year" db:"release_year"`
-    Language     *string   `json:"language" db:"language"`
-    SeasonCount  *int      `json:"season_count" db:"season_count"`
-    ThumbnailURL *string   `json:"thumbnail_url" db:"thumbnail_url"`
-    Banner       []byte    `json:"banner" db:"banner"` // MEDIUMBLOB
-    CreatedAt    time.Time `json:"created_at" db:"created_at"`
-    UpdatedAt    time.Time `json:"updated_at" db:"updated_at"`
+    ID           int       
+    Title        string    
+    Description  string    
+    ReleaseYear  int      
+    Language     string  
+    SeasonCount  int     
+    ThumbnailURL string  
+    Banner       []byte    
+    
 }
