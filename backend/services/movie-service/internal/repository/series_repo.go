e42634c544d616df02b98e4e0fa7137fb630cebb
Shanilka800@# <shanilka799@gmail.com>
2025-12-04 23:00:38 +0530

    create SaveSeries in series repo

--- backend/services/movie-service/internal/repository/series_repo.go
+++ backend/services/movie-service/internal/repository/series_repo.go
@@ -2,8 +2,8 @@ package repository
 
 import (
 	"database/sql"
-	"errors"
-
+	// "errors"                                                                      
+	
 	"github.com/DonShanilka/movie-service/internal/models"
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -12,4 +12,18 @@ type SeriesRepository struct {
 	DB *sql.DB
 }
 
-func (r *MovieRepository) SaveSeries(series models.Se)
+func (r *MovieRepository) SaveSeries(series models.Series) error {
+	quary := `INSERT INTO series (title, description, release_year, language, 					season_count, thumbnail_url, banner) VALUES (?, ?, ?, ?, ?, ?, ?)`
+
+	_, err := r.DB.Exec(quary,
+			series.Title,
+			series.Description,
+			series.ReleaseYear,
+			series.Language,
+			series.SeasonCount,
+			series.ThumbnailURL,
+			series.Banner,
+	)
+	return err
+
+}
