6aa001f5cc34f6e7b24b09adac6a7f6dcf67c52b
DonShanilka <shanilka799@gmail.com>
2025-12-17 13:39:45 +0530

    create tvSeries_service.go.go and create saveTvSeries

---
+++ backend/services/tvSeries-service/internal/Service/tvSeries_service.go
@@ -0,0 +1,43 @@
+package Repository
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/DonShanilka/movie-service/internal/Models"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type TvSeriesRepository struct {
+	SeriesColl   *mongo.Collection
+	EpisodesColl *mongo.Collection
+}
+
+func NewTvSeriesRepository(db *mongo.Database) *TvSeriesRepository {
+	return &TvSeriesRepository{
+		SeriesColl:   db.Collection("series"),
+		EpisodesColl: db.Collection("episodes"),
+	}
+}
+
+// ------------------ SERIES ------------------
+
+// Create a new TV series
+func (r *TvSeriesRepository) CreateSeries(series *Models.Series) (primitive.ObjectID, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	result, err := r.SeriesColl.InsertOne(ctx, series)
+	if err != nil {
+		return primitive.NilObjectID, err
+	}
+
+	id, ok := result.InsertedID.(primitive.ObjectID)
+	if !ok {
+		return primitive.NilObjectID, errors.New("failed to convert inserted ID to ObjectID")
+	}
+
+	return id, nil
+}
