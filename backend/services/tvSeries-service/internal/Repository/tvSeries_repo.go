126153b2f7aedc5120829f3622eebda87c0c8aaa
DonShanilka <shanilka799@gmail.com>
2025-12-17 22:25:30 +0530

    update series repo

--- backend/services/tvSeries-service/internal/Repository/tvSeries_repo.go
+++ backend/services/tvSeries-service/internal/Repository/tvSeries_repo.go
@@ -1,60 +1,18 @@
 package Repository
 
 import (
-	"context"
-	"errors"
-	"time"
-
 	"github.com/DonShanilka/tvSeries-service/internal/Models"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo"
+	"gorm.io/gorm"
 )
 
 type TvSeriesRepository struct {
-	SeriesColl *mongo.Collection
+	DB *gorm.DB
 }
 
-func NewTvSeriesRepository(db *mongo.Database) *TvSeriesRepository {
-	return &TvSeriesRepository{
-		SeriesColl: db.Collection("series"),
-	}
+func NewTvSeriesRepository(db *gorm.DB) *TvSeriesRepository {
+	return &TvSeriesRepository{DB: db}
 }
 
-// Create Series
-func (r *TvSeriesRepository) CreateSeries(series *Models.Series) (primitive.ObjectID, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	result, err := r.SeriesColl.InsertOne(ctx, series)
-	if err != nil {
-		return primitive.NilObjectID, err
-	}
-
-	id, ok := result.InsertedID.(primitive.ObjectID)
-	if !ok {
-		return primitive.NilObjectID, errors.New("failed to convert inserted ID")
-	}
-
-	return id, nil
-}
-
-// Add Season to existing Series
-func (r *TvSeriesRepository) AddSeasonToSeries(
-	seriesID primitive.ObjectID,
-	season Models.Season,
-) error {
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	filter := bson.M{"_id": seriesID}
-
-	update := bson.M{
-		"$push": bson.M{"seasons": season},
-		"$inc":  bson.M{"season_count": 1},
-	}
-
-	_, err := r.SeriesColl.UpdateOne(ctx, filter, update)
-	return err
+func (r *TvSeriesRepository) Create(tvSeries *Models.Series) error {
+	return r.DB.Create(tvSeries).Error
 }
