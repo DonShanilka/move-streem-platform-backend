f888473dfd86c4eb970820932f2da864acaf74e7
DonShanilka <shanilka799@gmail.com>
2025-12-20 13:38:14 +0530

    create User Service

--- backend/services/user-service/internal/Service/user_service.go
+++ backend/services/user-service/internal/Service/user_service.go
@@ -4,3 +4,27 @@ import (
 	"github.com/DonShanilka/user-service/internal/Models"
 	"github.com/DonShanilka/user-service/internal/Repository"
 )
+
+type UserService struct {
+	Repo *Repository.UserRepository
+}
+
+func NewAdminService(repo *Repository.UserRepository) *UserService {
+	return &UserService{Repo: repo}
+}
+
+func (service *UserService) CreateAdmin(admin *Models.User) error {
+	return service.Repo.CreateUser(admin)
+}
+
+func (service *UserService) UpdateAdmin(id uint, admin *Models.User) error {
+	return service.Repo.UpdateUser(id, admin)
+}
+
+func (service *UserService) DeleteAdmin(id uint) error {
+	return service.Repo.DeleteUser(id)
+}
+
+func (service *UserService) GetAllAdmins() ([]Models.User, error) {
+	return service.Repo.GetAllUser()
+}
