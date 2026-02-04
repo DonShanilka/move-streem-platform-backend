5c91c5531d087112bcc5efab94fb8b4f1d524d29
DonShanilka <shanilka799@gmail.com>
2026-02-04 22:53:00 +0530

    fix: Corrected typo in admin creation route and updated admin service listening port to 8083.

--- backend/services/admin-service/cmd/main.go
+++ backend/services/admin-service/cmd/main.go
@@ -25,8 +25,8 @@ func main() {
 	mux := http.NewServeMux()
 	Routes.RegisterAdminRoutes(mux, adminHandler)
 
-	log.Println("Admin Service running on :8080 🚀")
-	err = http.ListenAndServe(":8080", Middleware.CorsMiddleware(mux))
+	log.Println("Admin Service running on :8083 🚀")
+	err = http.ListenAndServe(":8083", Middleware.CorsMiddleware(mux))
 	if err != nil {
 		log.Fatal(err)
 	}
--- backend/services/admin-service/internal/Routes/routes.go
+++ backend/services/admin-service/internal/Routes/routes.go
@@ -7,7 +7,7 @@ import (
 )
 
 func RegisterAdminRoutes(mux *http.ServeMux, handler *Handler.AdminHandler) {
-	mux.HandleFunc("/api/admin/creatAdmin", handler.CreateAdmin)
+	mux.HandleFunc("/api/admin/createAdmin", handler.CreateAdmin)
 	mux.HandleFunc("/api/admin/getAllAdmins", handler.GetAllAdmins)
 	mux.HandleFunc("/api/admin/updateAdmin", handler.UpdateAdmin)
 	mux.HandleFunc("/api/admin/deleteAdmin", handler.DeleteAdmin)
